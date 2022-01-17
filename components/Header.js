a92897f559a305acb33dd7a67b5f330a3b2724d9
amr shiha <amrshiha2@gmail.com>
2022-01-17 22:18:25 +0200

    add header | responsive

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 function Header() {
     return (
         <header>
-            <div className="container py-4 flex justify-between items-center">
-                <Link href='/'><a className="font-poppins font-extrabold text-[26px] text-blue">Taskia</a></Link>
+            <div className="container py-4 flex justify-between items-center md:py-6">
+                <Link href='/'><a className="font-poppins font-extrabold text-[26px] text-blue md:text-[35px] 2xl:text-[47px] ">Taskia</a></Link>
                 <Link href='#'><a className="cta-btn">Try free</a></Link>
             </div>
         </header>
