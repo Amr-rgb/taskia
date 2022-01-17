d48c6b60bd897b44dab554a64f090a76e1d29a5a
amr shiha <amrshiha2@gmail.com>
2022-01-17 23:30:21 +0200

    add feedback | responsive

--- components/Feedback.js
+++ components/Feedback.js
@@ -1,9 +1,9 @@
 function Feedback() {
     return (
-        <section className="feedback-section my-16 mt-24 mx-4 pb-4 bg-[url('/users.png')] bg-contain bg-no-repeat bg-bottom">
+        <section className="feedback-section my-16 mt-24 mx-4 pb-4 bg-[url('/users.png')] bg-contain bg-no-repeat bg-bottom md:mx-14">
             <div className="container text-center">
                 <h2>Trust us and feel free to try our service</h2>
-                <p className="text-blue font-poppins font-extrabold text-[50px] py-10">100 000</p>
+                <p className="text-blue font-poppins font-extrabold text-[50px] py-10 md:text-[80px] md:py-20 lg:py-32 2xl:text-[150px] 2xl:py-40">100 000</p>
             </div>
         </section>
     );
