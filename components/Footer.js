bd1d00f524a5e3c5ca379a2fdb56bdcf6a6d8e4a
amr shiha <amrshiha2@gmail.com>
2022-01-17 23:35:09 +0200

    add footer | responsive

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
         <footer className="my-16 mt-32 text-center">
             <div className="container">
                 <h2>Get better work done</h2>
-                <p className="mt-2 font-muli font-light text-xs">See why millions of people across 195<br />countries use Dash.</p>
+                <p className="mt-2 font-muli font-light text-xs md:text-lg md:mt-4 2xl:text-[27px] 2xl:leading-8">See why millions of people across 195<br />countries use Dash.</p>
 
-                <div className="email-input mt-6">
-                    <input className="w-full mb-2" type="text" placeholder="Name@company.com" />
+                <div className="email-input mt-6 md:mt-8 2xl:mt-12">
+                    <input className="w-full mb-2 md:w-auto md:mb-0 md:mr-4" type="text" placeholder="Name@company.com" />
                     <Link href='#'><a className="btn">Try it free</a></Link>
                 </div>
             </div>
